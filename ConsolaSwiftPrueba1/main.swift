d48d93db7591498eab69af612cce66f8d9d5d8c8
Joselu Mac online 516 <jose_luis_fernandez_m-tsapp1ma2324@cev.com>
2024-09-25 09:32:03 +0100

    ya hemos validado para símbolos

--- ConsolaSwiftPrueba1/main.swift
+++ ConsolaSwiftPrueba1/main.swift
@@ -2,9 +2,8 @@
 //  main.swift
 //  ConsolaSwiftPrueba1
 //
-//  Created by user257493 on 9/24/24.
+//  Created by Eljoselu on 9/24/24.
 //
-
 import Foundation
 
 //VAR se usa para declarar variables que puedan variar a lo largo del codigo
@@ -14,23 +13,40 @@ let  picha = 10
 
 // error porque no es debilmente tipado:
 // num = "hola" ----- ERROR NO PUEDO ASIGNAR UN TIPO String  A UN int
+func tieneCaracteresEspeciales(texto: String) -> Bool {
+    let especiales = "¡!$%/()=¡¿?[]{}@"
+    for letra in texto {
+        for especial in especiales{
+            if letra == especial{
+                return true
+            }
+        }
+    }
+    return false
+}
+
+
 print("Introduce tu nombre")
 var nombre = readLine()!
 //print ("Tu nombre es:", nombre)
 var nombreIncorrecto = false
 repeat{
     if nombre.count >= 10{
-        print("ERROR 1- Nombre demasiado largo")
+        print("ERROR 1- Nombre demasiado largo, vuelve a introducirlo")
         nombreIncorrecto = true
         nombre = readLine()!
     }
     else if nombre.first!.isNumber{
-        print("ERROR 2- No se puede empezar con un número")
+        print("ERROR 2- No se puede empezar con un número, vuelve a introducirlo")
         nombreIncorrecto = true
+        nombre = readLine()!
     }
-    //else if nombre.contains(){
+    else if tieneCaracteresEspeciales(texto: nombre){
+        nombreIncorrecto = true
+        print("ERROR 3- El texto posee algún simbolo, vuelve a introducirlo")
+        nombre = readLine()!
         
-    //}
+    }
     else{
         print("Longitud correcta")
         nombreIncorrecto = false
